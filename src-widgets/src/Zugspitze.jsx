3b39654a5d192b4c8a39f44b7cc3b5d869e8c013
Stefan Pfaffe <pfaffe.st@gmail.com>
2024-04-30 14:55:33 +0200

    Add basic custom css

--- src-widgets/src/Zugspitze.jsx
+++ src-widgets/src/Zugspitze.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
 import { VisRxWidget } from '@iobroker/vis-2-widgets-react-dev';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/zugspitze.css';
 
 class Zugspitze extends (window.visRxWidget || VisRxWidget) {
     static getWidgetInfo() {
@@ -63,17 +63,8 @@ class Zugspitze extends (window.visRxWidget || VisRxWidget) {
         super.renderWidgetBody(props);
 
         return (
-            <>
-              <Button variant="primary">Primary</Button>{' '}
-              <Button variant="secondary">Secondary</Button>{' '}
-              <Button variant="success">Success</Button>{' '}
-              <Button variant="warning">Warning</Button>{' '}
-              <Button variant="danger">Danger</Button>{' '}
-              <Button variant="info">Info</Button>{' '}
-              <Button variant="light">Light</Button>{' '}
-              <Button variant="dark">Dark</Button>
-              <Button variant="link">Link</Button>
-            </>
+            <div id="wrapper">
+            </div>
         );
     }
 }
